2d398c94af97ee9d77f995f44ec0eb8fe916e9ad
danthedev <iamdanthedev@gmail.com>
2022-05-27 23:59:07 +0200

    update

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ChakraProvider } from '@chakra-ui/react'
-import { SigninForm } from './components/signin-form';
+console.log("initialized");
 
-const App = () => {
-    return (
-        <ChakraProvider>
-            <SigninForm onSignin={() => location.replace('/auth/redirect')} />
-        </ChakraProvider>
-    );
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { ChakraProvider } from '@chakra-ui/react'
+// import { SigninForm } from './components/signin-form';
+//
+// const App = () => {
+//     return (
+//         <ChakraProvider>
+//             <SigninForm onSignin={() => location.replace('/auth/redirect')} />
+//         </ChakraProvider>
+//     );
+// }
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
