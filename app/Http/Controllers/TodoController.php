3f8c4ef6906ba236b3723453b20d8a009a42e765
danthedev <iamdanthedev@gmail.com>
2022-05-27 18:53:41 +0200

    initial commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Get all user todos
+     * @return void
+     */
+    public function index() {
+//        $items = Todo::query()->where()
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $githubUser = Socialite::driver('github')->stateless()->user();
+
+    $user = User::updateOrCreate([
+        'github_id' => $githubUser->id
+    ], [
+        'name' => $githubUser->name,
+        'email' => $githubUser->email,
+        'github_id' => $githubUser->id,
+        'github_token' => $githubUser->token,
+        'github_refresh_token' => $githubUser->refreshToken,
+    ]);
+
+    Auth::login($user);
+
+    // $user->token
+
+    return redirect('/dashboard');
+});
